c8fee147cf714ade1d763c2c5f8505390776a18c
DeepKolos <deepkolos@qq.com>
2019-09-12 09:57:01 +0800

    1.0.3
    1.0.4
    
    1.0.5
    
    1.0.6
    
    1.0.7
    
    1.0.8
    
    1.0.9
    
    1.0.10
    
    1.0.11
    
    1.0.12
    
    1.0.13
    
    1.1.1
    
    1.1.2
    
    1.1.3
    
    1.1.4
    
    1.1.5

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ const fs = require('fs');
 const path = require('path');
 const ps = require('process');
 const pkg = require('./package.json');
+const copydirSync = require('./copyDir.js');
+const targetPkg = require(pkg._where + '/package.json');
 
-let pkgName;
-try {
-  pkgName = pkg.name.replace(/[\\\/]/g, '_');
-  if (!pkgName) new Error('package.json的name为空');
-} catch (error) {
-  throw new Error('读取package.json的name失败');
-}
-
-const dirUp = pkgName
-  .replace('\\', '/')
+const pkgName = targetPkg.name.replace(/[\\\/]/g, '_');
+const dirUp = targetPkg.name
+  .replace(/\\/g, '/')
   .split('/')
   .slice(1)
   .map(i => '../');
@@ -22,21 +17,12 @@ const dirUp = pkgName
 const src = ps.cwd();
 const desc = path.resolve(src, `../${dirUp}@types/${pkgName}`);
 
-fs.mkdirSync(desc, { recursive: true });
-
-fs.renameSync(src, desc);
-console.log('move dir from:', src);
-console.log('move dir to:', desc);
-
-// const files = fs.readdirSync(cwd);
-
-// files.forEach(file => {
-//   if (file !== 'node_modules')
-//     try {
-//       console.log('copyFile from:', cwd + '/' + file);
-//       console.log('copyFile to:', descPath + '/' + file);
-//       fs.copyFileSync(cwd + '/' + file, descPath + '/' + file);
-//     } catch (error) {
-//       console.log('TCL: error', error);
-//     }
-// });
+try {
+  fs.mkdirSync(desc, { recursive: true });
+  copydirSync(src, desc);
+  // fs.renameSync(src, desc);
+  console.log('copy dir from:', src);
+  console.log('copy dir to:', desc);
+} catch (error) {
+  console.log(error.toString());
+}
--- patch-script.js
+++ patch-script.js
@@ -2,21 +2,21 @@ const fs = require('fs');
 try {
   const projectPkg = require('../../package.json');
   console.log('patching project script');
-  if (projectPkg.scripts === undefined || !projectPkg.scripts.postinstall) {
-    projectPkg.scripts = {
-      ...projectPkg.scripts,
-      postinstall: 'move-to-types',
-    };
+  if (projectPkg.scripts === undefined) {
+    projectPkg.scripts = { postinstall: 'move-to-types' };
   } else {
     const currPostinstall = projectPkg.scripts.postinstall;
 
     projectPkg.scripts = {
       ...projectPkg.scripts,
-      postinstall: `${currPostinstall} & move-to-types`,
+      postinstall: currPostinstall
+        ? currPostinstall.indexOf('move-to-types') == -1
+          ? `${currPostinstall} & move-to-types`
+          : currPostinstall
+        : 'move-to-types',
     };
-
-    fs.writeFileSync('../../package.json', JSON.stringify(projectPkg, null, 2));
   }
+  fs.writeFileSync('../../package.json', JSON.stringify(projectPkg, null, 2));
 } catch (error) {
   console.log(error.toString());
 }
