6e0ae952689f10ce936162512ad4b69bdfb1dd70
DeepKolos <ouyanghaozhu@xiaomi.com>
2019-10-06 13:20:24 +0800

    去除package字段依赖

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
 const fs = require('fs');
 const path = require('path');
 const ps = require('process');
-const pkg = require(ps.cwd() + '/package.json');
 const copydirSync = require('./copyDir.js');
-const targetPkgPath = pkg._where + '/package.json';
-const targetPkg = require(targetPkgPath);
 
+const src = ps.cwd();
+const targetPkg = require(src + '/package.json');
 const pkgName =
   'at_' + targetPkg.name.replace(/[\\\/]/g, '_').replace(/@/g, '');
-const src = ps.cwd();
+
 const desc = path.resolve(
-  targetPkg._where,
-  `../node_modules/@types/${pkgName}`,
+  src,
+  `${
+    ~targetPkg.name.indexOf('@') ? '../../' : '../'
+  }../node_modules/@types/${pkgName}`,
 );
+
 try {
   fs.mkdirSync(desc, { recursive: true });
   copydirSync(src, desc);
