37f6c3a8ff49f1fc680eb3628f163e49989507ee
DeepKolos <ouyanghaozhu@xiaomi.com>
2019-10-06 14:41:37 +0800

    避免symbollink文件的复制， .bin目录

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const ps = require('process');
-const copydirSync = require('./copyDir.js');
+const fs = require('fs')
+const path = require('path')
+const ps = require('process')
+const copydirSync = require('./copyDir.js')
 
-const src = ps.cwd();
-const targetPkg = require(src + '/package.json');
-const pkgName =
-  'at_' + targetPkg.name.replace(/[\\\/]/g, '_').replace(/@/g, '');
+const src = ps.cwd()
+const targetPkg = require(src + '/package.json')
+const pkgName = 'at_' + targetPkg.name.replace(/[\\\/]/g, '_').replace(/@/g, '')
 
 const desc = path.resolve(
   src,
   `${
     ~targetPkg.name.indexOf('@') ? '../../../' : '../../'
   }../node_modules/@types/${pkgName}`,
-);
+)
 
 try {
-  fs.mkdirSync(desc, { recursive: true });
-  copydirSync(src, desc);
-  console.log('copy from:', src);
-  console.log('copy to:', desc);
+  console.log('copy from:', src)
+  console.log('copy to:', desc)
+  fs.mkdirSync(desc, { recursive: true })
+  copydirSync(src, desc, function(type, src, dir, basename) {
+    if (~src.indexOf('node_modules/.bin')) return false
+    return true
+  })
 } catch (error) {
-  console.log(error.toString());
+  console.log(error.toString())
 }
